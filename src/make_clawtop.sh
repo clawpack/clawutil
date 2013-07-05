9163fb4ff66911bfd497c58de9bf969cb304a70d
Randy LeVeque <rjl@ned>
2013-07-05 14:03:23 -0700

    modified make_clawtop.sh... ln -sf doesn't work for directories, so changed to -s. Added echo of what has been done

--- src/make_clawtop.sh
+++ src/make_clawtop.sh
@@ -7,10 +7,14 @@ GITDIR=`pwd`
 mkdir -p $CLAWTOP/clawpack
 cd $CLAWTOP/clawpack
 echo " " > __init__.py
-ln -sf $GITDIR/pyclaw/src/pyclaw pyclaw
-ln -sf $GITDIR/visclaw/src/python/visclaw visclaw
-ln -sf $GITDIR/riemann/src/python/riemann riemann
-ln -sf $GITDIR/clawutil/src/python/clawutil clawutil
-ln -sf $GITDIR/amrclaw/src/python/amrclaw amrclaw
-ln -sf $GITDIR/geoclaw/src/python/geoclaw geoclaw
+ln -s $GITDIR/pyclaw/src/pyclaw pyclaw
+ln -s $GITDIR/visclaw/src/python/visclaw visclaw
+ln -s $GITDIR/riemann/src/python/riemann riemann
+ln -s $GITDIR/clawutil/src/python/clawutil clawutil
+ln -s $GITDIR/amrclaw/src/python/amrclaw amrclaw
+ln -s $GITDIR/geoclaw/src/python/geoclaw geoclaw
 cd $GITDIR
+
+echo Created python/clawpack directory with symbolic links to python dirctories
+echo Modify your PYTHONPATH to include $CLAW/python 
+echo        so you can import clawpack modules
