41998abd9e88c3037fe095026753185a2216ad88
Randy LeVeque <rjl@uw.edu>
2014-12-30 21:11:02 -0800

    simplied nbtools to just take label, and added make_data, make_exe(new=True)

--- src/python/clawutil/nbtools.py
+++ src/python/clawutil/nbtools.py
@@ -31,14 +31,36 @@ def make_htmls(outfile=None, verbose=False, readme_link=True):
         print "See the README.html file for links to input files..."
         display(FileLink('README.html'))
     
-
-def make_exe(outfile=None, verbose=True):
-    """Perform 'make .exe' and display link."""
+def make_data(verbose=True):
+    """Perform 'make data' and display link."""
     import os,sys
     
-    if outfile is None:
-        outfile='compile_output.txt'
-    cmd = 'make .exe &> %s' % outfile
+    outfile='data_output.txt'
+    cmd = 'make data &> %s' % outfile
+
+    if verbose:
+        print "Making Fortran data files... %s" % cmd
+        sys.stdout.flush()
+
+    status = os.system(cmd)
+    
+    if verbose:
+        local_file = FileLink(outfile)
+        print "Done...  Check this file to see output:" 
+        display(local_file)
+
+def make_exe(new=False, verbose=True):
+    """
+    Perform 'make .exe' and display link.
+    If *new = True*, do 'make new' instead to force recompilation of everything.
+    """
+    import os,sys
+    
+    outfile='compile_output.txt'
+    if new:
+        cmd = 'make new &> %s' % outfile
+    else:
+        cmd = 'make .exe &> %s' % outfile
 
     if verbose:
         print "Compiling code... %s" % cmd
@@ -51,18 +73,20 @@ def make_exe(outfile=None, verbose=True):
         print "Done...  Check this file to see output:" 
         display(local_file)
     
-def make_output(outdir=None, outfile=None, verbose=True):
+def make_output(label=None, verbose=True):
     """Perform 'make output' and display link."""
     import os,sys
     
-    cmd = 'make output'
-    if outdir is not None:
-        cmd = cmd + ' OUTDIR=%s' % outdir
-    if outfile is None:
-        outfile = 'run_output.txt'
-        cmd = cmd + ' &> %s' % outfile
+    if label is None: 
+        label = ''
     else:
-        cmd = cmd + ' &> %s' % outfile
+        if label[0] != '_':
+            label = '_' + label
+    outdir = '_output%s' % str(label)
+    outfile = 'run_output%s.txt' % str(label)
+
+    cmd = 'make output OUTDIR=%s &> %s' % (outdir,outfile)
+
     if verbose:
         print "Running code... %s" % cmd
         sys.stdout.flush()
@@ -72,26 +96,23 @@ def make_output(outdir=None, outfile=None, verbose=True):
         local_file = FileLink(outfile)
         print "Done... Check this file to see output:"
         display(local_file)
+
+    return outdir
     
-def make_plots(outdir=None, plotdir=None, outfile=None, verbose=True):
+def make_plots(label=None, verbose=True):
     """Perform 'make plots' and display links"""
     import os, sys
 
-    cmd = 'make plots'
-    if outdir is not None:
-        cmd = cmd + ' OUTDIR=%s' % outdir
-
-    if plotdir is None:
-        plotdir = '_plots'
+    if label is None: 
+        label = ''
     else:
-        cmd = cmd + ' PLOTDIR=%s' % plotdir
+        if label[0] != '_':
+            label = '_' + label
+    outdir = '_output%s' % str(label)
+    plotdir = '_plots%s' % str(label)
+    outfile = 'plot_output%s.txt' % str(label)
 
-    if outfile is None:
-        outfile = 'plot_output.txt'
-        cmd = cmd + ' &> %s' % outfile
-    else:
-        cmd = cmd + ' &> %s' % outfile
-
+    cmd = 'make plots OUTDIR=%s PLOTDIR=%s  &> %s' % (outdir,plotdir,outfile)
 
     if verbose:
         print "Making plots... %s" % cmd
@@ -107,6 +128,8 @@ def make_plots(outdir=None, plotdir=None, outfile=None, verbose=True):
         index_file = FileLink('%s/_PlotIndex.html' % plotdir)
         print "View plots created at this link:"
         display(index_file)
+
+    return plotdir
     
 
 def make_output_and_plots(label=None, verbose=True):
@@ -117,11 +140,7 @@ def make_output_and_plots(label=None, verbose=True):
     else:
         if label[0] != '_':
             label = '_' + label
-    outdir = '_output%s' % str(label)
-    outfile = 'run_output%s.txt' % str(label)
-    make_output(outdir,outfile,verbose)
 
-    plotdir = '_plots%s' % str(label)
-    outfile = 'plot_output%s.txt' % str(label)
-    make_plots(outdir,plotdir,outfile,verbose)
-    return plotdir
+    outdir = make_output(label,verbose)
+    plotdir = make_plots(label,verbose)
+    return outdir,plotdir
