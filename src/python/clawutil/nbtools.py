04f5df5815695b37da4a8fe8990dfa7c805d49d1
Randy LeVeque <rjl@uw.edu>
2014-12-31 17:49:48 -0800

    modified nbtools.py to use subprocess rather than os.system and cleaned up via make_driver function

--- src/python/clawutil/nbtools.py
+++ src/python/clawutil/nbtools.py
@@ -9,73 +9,84 @@ except:
     print "*** Ipython version does not support FileLink"
 
 
-def make_htmls(outfile=None, verbose=False, readme_link=True):
-    """Perform 'make .htmls' and display link."""
-    import os,sys
+def make_driver(args, env, outfile, verbose):
+
+    """
+    Use subprocess to run a make command and capture the output.
+
+    :Input:
+    - *args*: arguments to the make command
+    - *env*: environment to run in (if None, use *os.environ*)
+      Useful if $CLAW must be set in notebook.
+    - *outfile*: file name of file for capturing stdout and stderr
+    - *vervose*: if True, print out command and display link to output file
     
-    if outfile is None:
-        outfile='htmls_output.txt'
-    cmd = 'make .htmls &> %s' % outfile
+    If the return code is non-zero, print a warning and link to output file
+    regardless of value of *verbose*.
+
+    """
+
+    import subprocess
+    import os, sys
+
+    if env is None:
+        env = os.environ
 
     if verbose:
-        print "Making html documentation files... %s" % cmd
+        print "Executing shell command:   make %s" % args
         sys.stdout.flush()
 
-    status = os.system(cmd)
+    ofile = open(outfile,'w')
+    cmd_list = ['make'] + args.split()
+    job = subprocess.Popen(cmd_list, stdout=ofile,stderr=ofile,env=env)
+    return_code = job.wait()
+    errors = (return_code != 0)
+    if errors:
+        print "*** Possible errors, return_code = %s" % return_code
     
-    if verbose:
+    if verbose or errors:
         local_file = FileLink(outfile)
         print "Done...  Check this file to see output:" 
         display(local_file)
+
+def make_htmls(outfile=None, env=None, verbose=False, readme_link=True):
+    """Perform 'make .htmls' and display link."""
+    
+    if outfile is None:
+        outfile='htmls_output.txt'
+
+    args = '.htmls'
+
+    make_driver(args, env, outfile, verbose)
+
     if readme_link:
         print "See the README.html file for links to input files..."
         display(FileLink('README.html'))
     
-def make_data(verbose=True):
+def make_data(env=None, verbose=True):
     """Perform 'make data' and display link."""
-    import os,sys
     
     outfile='data_output.txt'
-    cmd = 'make data &> %s' % outfile
+    args = 'data'
+    make_driver(args, env, outfile, verbose)
 
-    if verbose:
-        print "Making Fortran data files... %s" % cmd
-        sys.stdout.flush()
-
-    status = os.system(cmd)
-    
-    if verbose:
-        local_file = FileLink(outfile)
-        print "Done...  Check this file to see output:" 
-        display(local_file)
-
-def make_exe(new=False, verbose=True):
+def make_exe(new=False, env=None, verbose=True):
     """
     Perform 'make .exe' and display link.
     If *new = True*, do 'make new' instead to force recompilation of everything.
     """
-    import os,sys
     
     outfile='compile_output.txt'
     if new:
-        cmd = 'make new &> %s' % outfile
+        args = 'new'
     else:
-        cmd = 'make .exe &> %s' % outfile
+        args = '.exe'
 
-    if verbose:
-        print "Compiling code... %s" % cmd
-        sys.stdout.flush()
+    make_driver(args, env, outfile, verbose)
 
-    status = os.system(cmd)
     
-    if verbose:
-        local_file = FileLink(outfile)
-        print "Done...  Check this file to see output:" 
-        display(local_file)
-    
-def make_output(label=None, verbose=True):
+def make_output(label=None, env=None, verbose=True):
     """Perform 'make output' and display link."""
-    import os,sys
     
     if label is None: 
         label = ''
@@ -85,23 +96,14 @@ def make_output(label=None, verbose=True):
     outdir = '_output%s' % str(label)
     outfile = 'run_output%s.txt' % str(label)
 
-    cmd = 'make output OUTDIR=%s &> %s' % (outdir,outfile)
-
-    if verbose:
-        print "Running code... %s" % cmd
-        sys.stdout.flush()
-    status = os.system(cmd)
-    
-    if verbose:
-        local_file = FileLink(outfile)
-        print "Done... Check this file to see output:"
-        display(local_file)
+    args = 'output OUTDIR=%s' % outdir
+    make_driver(args, env, outfile, verbose)
 
     return outdir
+
     
-def make_plots(label=None, verbose=True):
+def make_plots(label=None, env=None, verbose=True):
     """Perform 'make plots' and display links"""
-    import os, sys
 
     if label is None: 
         label = ''
@@ -112,19 +114,10 @@ def make_plots(label=None, verbose=True):
     plotdir = '_plots%s' % str(label)
     outfile = 'plot_output%s.txt' % str(label)
 
-    cmd = 'make plots OUTDIR=%s PLOTDIR=%s  &> %s' % (outdir,plotdir,outfile)
+    args = 'plots OUTDIR=%s PLOTDIR=%s' % (outdir,plotdir)
+    make_driver(args, env, outfile, verbose)
 
     if verbose:
-        print "Making plots... %s" % cmd
-        sys.stdout.flush()
-
-    status = os.system(cmd)
-    
-    if verbose:
-        local_file = FileLink(outfile)
-        print "Done... Check this file to see output:"
-        display(local_file)
-    
         index_file = FileLink('%s/_PlotIndex.html' % plotdir)
         print "View plots created at this link:"
         display(index_file)
@@ -132,15 +125,8 @@ def make_plots(label=None, verbose=True):
     return plotdir
     
 
-def make_output_and_plots(label=None, verbose=True):
+def make_output_and_plots(label=None, env=None, verbose=True):
 
-    import sys
-    if label is None: 
-        label = ''
-    else:
-        if label[0] != '_':
-            label = '_' + label
-
-    outdir = make_output(label,verbose)
-    plotdir = make_plots(label,verbose)
+    outdir = make_output(label,env,verbose)
+    plotdir = make_plots(label,env,verbose)
     return outdir,plotdir
