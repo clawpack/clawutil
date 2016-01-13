d9a256bb01017106789fa546dfd6d366ef6b29de
Kyle Mandli <kyle.mandli@gmail.com>
2016-01-12 19:31:02 -0600

    Refactor and bug fix
    Use built-in functionality from standard Python modules to perform
    a number of functions from the original implementation.  Also clean up
    code significantly.  Note that this modifies the order of the resulting
    source list to put the included source last rather than first.  This
    does not work in general due to module dependency order problems.

--- src/checkSources.py
+++ src/checkSources.py
@@ -1,51 +1,57 @@
-#quick script to consolidate common and custom sources
-#If a custom file has the same name as a common file, the common one is excluded.
-#Exclusions can also be manually set for replacement files with different names
+#!/usr/bin/env python
+r"""
+Consolidate common and custom sources into one source list.
+
+If a custom file has the same name as a common file, the common one is excluded.
+Exclusions can also be manually set for replacement files with different names.
+
+NOTE:  Unfortunately we may need to implement something that allows flexibility
+as to when a module is compiled and available due to depedency issues
+"""
+
+from __future__ import print_function
 
 from __future__ import absolute_import
 from __future__ import print_function
 import sys
 import os
 
+def consolidate_src_lists(common_src_files, excluded_src_files, src_files):
+    r"""
+    """
 
-#extract just the file name
-def get_name(filedir):
-	return str(filedir).split('/')[-1].split('.')[0]
+    excluded_src_names = [os.path.basename(os.path.splitext(src_file)[0]) 
+                          for src_file in excluded_src_files]
+    included_src_names = [os.path.basename(os.path.splitext(src_file)[0]) 
+                          for src_file in src_files]
+
+    src_list = []
+    for src_file in common_src_files:
+        if os.path.basename(os.path.splitext(src_file)[0]) not in excluded_src_names and \
+           os.path.basename(os.path.splitext(src_file)[0]) not in included_src_names:
+            src_list.append(src_file)
+
+    for src_file in src_files:
+        src_list.append(src_file)
+
+    return src_list
 
 
-#first, make arrays for sources
-sources=[]
-s_names=[]
+def parse_args(arg_list):
+    r"""
+    """
+    file_list = [[], [], []]
+    list_index = 0
 
-all_sources=list(sys.argv)
-all_sources.pop(0)
-mode=0
-for arg in all_sources:
-	if(arg==";"):
-		mode+=1
-		continue
-	
-	#common file
-	if mode==0:
-		sources.append(arg)
-		s_names.append(get_name(arg))
-	
-	#overwritten file
-	else:
-		exname=get_name(arg)
-		#check if there is a file to replace
-		if exname in s_names:
-			ind=s_names.index(exname)
-			sources.pop(ind)
-			s_names.pop(ind)
-		
-		#custom file
-		if mode==2:
-			sources.append(arg)
-			s_names.append(get_name(arg))
+    for arg in arg_list:
+        if arg == ";":
+            list_index += 1
+        else:
+            file_list[list_index].append(arg)
 
-#return the consolidated source list
-out=""
-for source in sources:
-	out+=" "+source
-print(out)
+    return file_list[0], file_list[1], file_list[2]
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        raise ValueError("Need to provide at least one source.")
