8e158522fedc89f9f55cedb101a6a74abb894909
Randy LeVeque <rjl@uw.edu>
2025-12-29 11:50:05 -0800

    rename setrun_cases.py to setrun_case.py and same for setplot

--- src/python/clawutil/clawmultip_tools.py
+++ src/python/clawutil/clawmultip_tools.py
@@ -285,20 +285,21 @@ def make_cases_template():
         case['xclawcmd'] = 'xclaw'  # executable created by 'make .exe'
 
         # setrun parameters:
-        case['setrun_file'] = 'setrun_cases.py'
-        # setrun_cases.py should contain a setrun function with case
+        case['setrun_file'] = 'setrun_case.py'
+        # setrun_case.py should contain a setrun function with case
         # as a keyword argument so we can pass in parameters
 
         #case['plotdir'] = None  # if None, will not make plots
         case['plotdir'] = '_plots_%s' % case_name
 
-        case['setplot_file'] = 'setplot_cases.py'
-        # setplot_cases.py might contain a setplot function with case
+        case['setplot_file'] = 'setplot_case.py'
+        # setplot_case.py might contain a setplot function with case
         # as a keyword argument so we can pass in parameters
 
         # ADD CASE ENTRIES for any setrun or setplot parameters that
         # are case-dependent, using this format:
         # case[key] = value   # for each parameter
+        # and then modify setrun_case.py and/or setplot_case.py to use them
 
         caselist.append(case)
 
